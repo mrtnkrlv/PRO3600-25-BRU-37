05cc2230a87f6b8de5e25caf64de481a15b209f2
Paulito1546 <paul.emptoz@gmail.com>
2025-04-04 19:28:26 +0200

    Modification of comentary.js into coment.js and table_commentary

--- Backend/tbl_commentary.sql
+++ Backend/tbl_commentary.sql
@@ -1,18 +1,18 @@
 USE mainDb;
 
 
-CREATE TABLE commentary (
-    commentaryId INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE comments (
+    commentId INTEGER AUTO_INCREMENT PRIMARY KEY,
     mealId INTEGER NOT NULL,
     userId VARCHAR(255) NOT NULL,  
-    commentaryParentId INTEGER, -- to be able to respond to a commentary 
+    commentParentId INTEGER, -- to be able to respond to a commentary 
     dateCreation DATE DEFAULT (CURRENT_DATE),
     content TEXT NOT null,
     likes INTEGER DEFAULT 0,
     dislikes INTEGER DEFAULT 0,
     FOREIGN KEY (mealId) REFERENCES meals(mealId), 
     FOREIGN KEY (userId) REFERENCES user(id),
-    FOREIGN KEY (commentaryParentId) REFERENCES commentary(commentaryId) 
+    FOREIGN KEY (commentParentId) REFERENCES comments(commentId) 
 );
 
 
