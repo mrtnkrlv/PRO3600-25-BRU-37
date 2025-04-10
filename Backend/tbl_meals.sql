08f1943192074ab1be92e33a114662854920b886
Paulito1546 <paul.emptoz@gmail.com>
2025-04-10 22:47:18 +0200

    changement

--- Backend/tbl_meals.sql
+++ Backend/tbl_meals.sql
@@ -7,5 +7,6 @@ CREATE TABLE meals (
     mealName VARCHAR(255) NOT NULL,
     likes INTEGER DEFAULT 0,
     positionInWeek INTEGER -- Between 1 and 5, represents day of the week (meals are refreshed weekly)
-
+    averageRating DECIMAL(3,2) DEFAULT 0.00,
+    ratingCount INT DEFAULT 0   
 );
