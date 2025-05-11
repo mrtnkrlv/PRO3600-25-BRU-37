b00acd295b4e9c79fc04e8474b8a72a2154d0eab
Paulito1546 <paul.emptoz@gmail.com>
2025-05-11 17:28:09 +0200

    Début de la documentation

--- Server/database.js
+++ Server/database.js
@@ -2,6 +2,14 @@ import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 dotenv.config(); // No path needed
 
+/**
+ * @module database
+ */
+
+/**
+ * Pool de connexions MySQL pour accéder à la base de données.
+ * @type {object}
+ */
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
